c86b8e9f1a973c236790030f28978d3bd295fa96
sanjeeth-singh <sanjeethsinghthakur0090@gmail.com>
2024-01-03 17:51:20 +0530

    first commit

---
+++ arrob.js
@@ -0,0 +1,177 @@
+var matt = [
+    { 
+    
+        icon:"bi bi-camera-reels",
+        head:'Daily Class Video Recordings',
+        details:'Daily class videos are recorded and made available for all students for revisiting the topics.'
+    
+    },
+    { 
+    
+        icon:'bi bi-person-video3',
+        head:'Highly Experienced Trainers',
+        details:'Our trainers are highly experienced and have ample knowledge of industry level standards.'
+    
+    },
+    { 
+    
+        icon:"bi bi-cast",
+        head:'Class Program Options',
+        details:'We provide both online and offline classes with assured learning assistance.'
+    
+    },
+    { 
+    
+        icon:"bi bi-journal-richtext",
+        head:'Several High Quality Courses',
+        details:'All our courses are of the best quality and provide immense exposure to the industry level practices.'
+    
+    },  
+    { 
+    
+        icon:"bi bi-star",
+        head:'Earn a Certificate of Achievement',
+        details:'We provide internship and training certificates on successful completion of the courses.'
+    
+    }
+]
+
+var courseImg = [{img:'https://edupoly.in/common/assets/HTML.jpg',course:'HTML',numcourse:'4 courses'},
+{img:'https://edupoly.in/common/assets/CSS.jpg',course:'CSS',numcourse:'4 courses'},
+{img:'https://edupoly.in/common/assets/Javascript.jpg',course:'JavaScript',numcourse:'4 courses'},
+{img:'https://edupoly.in/common/assets/Angular.jpg',course:'Angular',numcourse:'3 courses'},
+{img:'https://edupoly.in/common/assets/React.jpg',course:'ReactJS',numcourse:'3 courses'},
+{img:'https://edupoly.in/common/assets/NodeJS.jpg',course:'NodeJS',numcourse:'2 courses'},
+{img:'https://edupoly.in/common/assets/Express.jpg',course:'ExpressJS',numcourse:'2 courses'},
+{img:'https://edupoly.in/common/assets/Mongo%20db.png',course:'MongoDB',numcourse:'2 courses'},
+{img:'https://edupoly.in/common/assets/java.png',course:'Java',numcourse:'1 courses'},
+{img:'https://edupoly.in/common/assets/Spring.jpg',course:'Spring',numcourse:'1 courses'},
+{img:'https://edupoly.in/common/assets/python_1.jpg',course:'Python',numcourse:'1 courses'},
+{img:'https://edupoly.in/common/assets/django.jpg',course:'Django',numcourse:'1 courses'}]
+
+var topCourse = [
+    {
+        img:'https://edupoly.in/common/assets/angular.svg',
+        title:'Frontend Web Development with Angular',
+        description:'500+ assignments and 2 projects',
+        price:20000,
+        duration:'50 hours',
+        level : 'All level',
+        months:'3 months'
+    },
+    {
+        img:'https://edupoly.in/common/assets/react.svg',
+        title:'Frontend Web Development with ReactJS',
+        description:'500+ assignments and 2 projects',
+        price:20000,
+        duration:'50 hours',
+        level : 'All level',
+        months:'3 months'
+    },
+    {
+        img:'https://edupoly.in/common/assets/Mean.svg',
+        title:'Full Stack Web Development with MEAN',
+        description:'500+ assignments and projects',
+        price:30000,
+        duration:'60 hours',
+        level : 'Intermediate',
+        months:'4 months'
+    },
+    {
+        img:'https://edupoly.in/common/assets/MERN.svg',
+        title:'Full Stack Web Development with MERN',
+        description:'500+ assignments and projects',
+        price:30000,
+        duration:'60 hours',
+        level : 'Intermediate',
+        months:'4 months'
+    },
+    {
+        img:'https://edupoly.in/common/assets/programming-with-java.svg',
+        title:'Java Full Stack Development Programme',
+        description:'500+ assignments and projects',
+        price:30000,
+        duration:'60 hours',
+        level : 'Intermediate',
+        months:'4 months'
+    },
+    {
+        img:'https://edupoly.in/common/assets/programming-with-python.svg',
+        title:'Python Full Stack Development Programme',
+        description:'500+ assignments and projects',
+        price:30000,
+        duration:'60 hours',
+        level : 'Intermidiate',
+        months:'4 months'
+    }
+
+]
+
+var feedbackArr = [
+    {
+        'icon':'bi bi-person-circle',
+        'name':'Nenavath Naresh',
+        'feedback':'The environment of institute creates for getting knowldge on web technologies and provides best knowledge for the students and a lot practice assignment helps very much for the students.'
+    },
+    {
+        'icon':"bi bi-person-circle",
+        'name':'Nenavath Suresh',
+        'feedback':'The environment of institute creates for getting knowldge on web technologies and provides best knowledge for the students and a lot practice assignment helps very much for the students.'
+    },
+    {
+        'icon':"bi bi-person-circle",
+        'name':'Nenavath Divya',
+        'feedback':'The environment of institute creates for getting knowldge on web technologies and provides best knowledge for the students and a lot practice assignment helps very much for the students.'
+    },
+    {
+        'icon':"bi bi-person-circle",
+        'name':'Nenavath Tharun',
+        'feedback':'The environment of institute creates for getting knowldge on web technologies and provides best knowledge for the students and a lot practice assignment helps very much for the students.'
+    },
+   
+    {
+        'icon':"bi bi-person-circle",
+        'name':'Eslavath Srinu',
+        'feedback':'The environment of institute creates for getting knowldge on web technologies and provides best knowledge for the students and a lot practice assignment helps very much for the students.'
+    },
+    {
+        'icon':"bi bi-person-circle",
+        'name':'Eslavath Siddu',
+        'feedback':'The environment of institute creates for getting knowldge on web technologies and provides best knowledge for the students and a lot practice assignment helps very much for the students.'
+    },
+]
+
+var contactUs = [
+    {
+        'conct' : 'WatsApp Number',
+        'info':'9561214818',
+        'con': 'https://static-00.iconduck.com/assets.00/whatsapp-icon-2040x2048-8b5th74o.png'
+    },
+    {
+        'conct' : 'Email',
+        'info':'info.edupoly@gmail.com',
+        'con': 'https://static-00.iconduck.com/assets.00/mail-icon-2048x2048-525ey8hu.png'
+    },
+    {
+        'conct' : 'Youtube',
+        'info':'Praveen classes',
+        'con': 'https://static-00.iconduck.com/assets.00/youtube-icon-2048x2048-879wd8sv.png'
+    },
+    {
+        'conct' : 'LinkedIn',
+        'info':'Edupoly',
+        'con': 'https://static-00.iconduck.com/assets.00/linkedin-icon-2048x2048-ya5g47j2.png'
+    },
+    {
+        'conct' : 'Telegram',
+        'info':'Praveen Classes',
+        'con': 'https://w7.pngwing.com/pngs/607/1001/png-transparent-white-arrow-illustration-telegram-logo-computer-icons-social-miscellaneous-blue-angle-thumbnail.png'
+    },
+    {
+        'conct' : 'Instagram',
+        'info':'edu.poly',
+        'con': 'https://i0.wp.com/tbrsteakhouse.com/wp-content/uploads/2018/11/instagram-colourful-icon.png?ssl=1'
+    },
+   
+   
+]
